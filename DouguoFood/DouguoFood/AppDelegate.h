06a1529ed6a23a8f5fa2d560bb1fc3148e425297
SoSee-Mac-1 <15911001671@163.com>
2016-03-09 11:58:37 +0800

    1

---
+++ DouguoFood/DouguoFood/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  DouguoFood
+//
+//  Created by mac on 15/9/25.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+/****又一个更改***/
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DouguoFood/DouguoFood/中间的视图/圈圈/Controller/FindViewController.h
@@ -0,0 +1,15 @@
+//
+//  FindViewController.h
+//  DouguoFood
+//
+//  Created by mac on 15/9/25.
+//  Copyright (c) 2015年 mac. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface FindViewController : BaseViewController
+
+/*这是新的更改*/
+
+@end
